9576b184f44ace11ed381d538e2ee75a2b0d286a
Gabriel Lins <101433324+linsglf@users.noreply.github.com>
2023-10-13 01:57:20 -0300

    Created Get Tasks

---
+++ todolist/src/main/java/br/com/cursojavars/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,62 @@
+package br.com.cursojavars.todolist.filter;
+
+
+
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.cursojavars.todolist.users.IUserRepository;
+import br.com.cursojavars.todolist.users.UserModel;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository UserRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+        if (servletPath.equals("/tasks/")) {
+            var authorization = request.getHeader("Authorization");
+
+            var authEncoded = authorization.substring("Bacic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecode);
+
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            var user = this.UserRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    System.out.println("ID USER: " + user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+            }
+        } else {
+            filterChain.doFilter(request, response);
+            System.out.println("SERVLET PATH: " + servletPath);
+        }
+    }
+}
---
+++ todolist/src/main/java/br/com/cursojavars/todolist/task/TaskController.java
@@ -0,0 +1,46 @@
+package br.com.cursojavars.todolist.task;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de início/término não pode ser menor que a data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de início não pode ser maior que a data de término");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+        return tasks;
+    }
+}
